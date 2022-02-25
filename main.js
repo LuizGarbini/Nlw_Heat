2f677eee1f0a69b2dc2238b12ab8a5a82fcb14c0
Luiz Garbini <garbasneto@gmail.com>
2022-02-24 22:43:00 -0300

    :sparkles: JS/HTML

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: "LuizGarbini",
+  youtube: "channel/UCuOQFn0zMGz9AvDhTUe7hVQ",
+  facebook: "luizgarbinineto.garbini",
+  instagram: "luizz3ra",
+  twitter: "luizgnetoo",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
+}
+
+changeSocialMediaLinks();
+
+function getGitGubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      UserImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
+getGitGubProfileInfos();
